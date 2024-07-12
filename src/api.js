864780938cc056385e609b842d45714b2a44f605
“Omkar <omkarkhadakkar13@gmail.com>
2024-07-12 23:42:43 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e4aa31eea7msh747d817ddd9f690p11f94djsn79b5c715bf07",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0601cef877001bfd9b33dd3eb30b3ead"; 
